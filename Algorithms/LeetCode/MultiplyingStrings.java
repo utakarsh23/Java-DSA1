103f3fc3a480b527ec9bf91b4aac755b0cafc8d4
utakarsh23 <utakarsh.t@somaiya.edu>
2024-12-17 00:38:03 +0530

    hii

--- Algorithms/LeetCode/MultiplyingStrings.java
+++ Algorithms/LeetCode/MultiplyingStrings.java
@@ -1,26 +1,37 @@
 package LeetCode;
+
+import java.math.BigInteger;
+
 //https://leetcode.com/problems/multiply-strings/
 public class MultiplyingStrings {
     public static void main(String[] args) {
-        String a = "12";
-        String b = "456";
-        System.out.println(intconv(a, b));
+        String a = "498828660196";
+        String b = "840477629533";
+        System.out.println(INS(a, b));
 
     }
-    static int intconv(String num1, String num2){
-        int i = 0;
-        int l = 0;
-        int len = num1.length();
-        for (int j = 0; j < len; j++) {
-            char k = (num1).charAt(j);
-            i = i*10+(int)k-48;
-        }
-        len = num2.length();
-        for (int j = 0; j < len; j++) {
-            char m = (num2).charAt(j);
-            l = l*10+(int)m-48;
-        }
-        return i*l;
+//    static String intconv(String num1, String num2){
+//        BigInteger i = new BigInteger(String.valueOf(0));
+//        BigInteger l = new BigInteger(String.valueOf(0));
+//        long len = num1.length();
+//        for (int j = 0; j < len; j++) {
+//            char k = (num1).charAt(j);
+//            i = i*10+(int)k-48;
+//        }
+//        len = num2.length();
+//        for (int j = 0; j < len; j++) {
+//            char m = (num2).charAt(j);
+//            l = l*10+(int)m-48;
+//        }
+//        long ss = (i*l);
+//        String t = ss+" ";
+//        return t;
+//    }
+    static String INS(String num1, String num2){
+        BigInteger a = new BigInteger(num1);
+        BigInteger b = new BigInteger(num2);
+        BigInteger ab = a.multiply(b);
+        return ab.toString();
     }
 
 }
---
+++ Algorithms/LeetCode/Sqrt.java
@@ -0,0 +1,46 @@
+package LeetCode;
+
+public class Sqrt {
+    public static void main(String[] args) {
+        System.out.println(sqrt1(49));
+    }
+    static double sqrt(double x) {
+        double low = 0;
+        double high = x;
+        while(low<=high) {
+            double mid = high+(low-high)/2;
+            if(mid*mid == x) {
+                return mid;
+            } else if (mid*mid< x) {
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        int r = (int)high;
+        return high;
+    }
+    static boolean sqrt1(int n)
+    {
+        if (n < 2) {
+            return false;
+        }
+        int low = 1, high = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid * mid == n) {
+                return true;
+            }
+            else if (mid * mid < n) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        if(high*high == n) {
+            return true;
+        }
+        return false;
+    }
+}
