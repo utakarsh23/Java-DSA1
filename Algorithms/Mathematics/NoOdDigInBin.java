7f299f3dc01b467090a1e29ff6e37f345adcf751
utakarsh23 <utakarsh.t@somaiya.edu>
2024-12-18 12:22:20 +0530

    hii

--- Algorithms/Mathematics/NoOdDigInBin.java
+++ Algorithms/Mathematics/NoOdDigInBin.java
@@ -2,17 +2,20 @@ package Mathematics;
 
 public class NoOdDigInBin {
     public static void main(String[] args) {
-        System.out.println(mySqrt(-25));
+        System.out.println(mySqrt(2147395599));
+        //2147395599
+        //2147483647
+//        System.out.println(Integer.MAX_VALUE);
     }
     static int noOfDig(int num, int base) {
         return  (int)((Math.log(num)/Math.log(base)) +1);
         //Log(num)base(2)+1
     }
-    static int mySqrt(int x) {
-        int left = 0;
-        int right = x;
+    static long mySqrt(long x) {
+        long left = 0;
+        long right = x;
         while(left<=right) {
-            int mid = left+(right-left)/2;
+            long mid = left+(right-left)/2;
             if(mid*mid == x) return mid;
             if(mid*mid < x) {
                 left = mid+1;
@@ -20,6 +23,6 @@ public class NoOdDigInBin {
                 right = mid-1;
             }
         }
-        return right;
+        return left;
     }
 }
