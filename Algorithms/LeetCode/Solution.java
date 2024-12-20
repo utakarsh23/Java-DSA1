2900eeb387568e17be589b5102b0a1d1dcc15025
utakarsh23 <utakarsh.t@somaiya.edu>
2024-12-20 23:45:25 +0530

    hii

---
+++ Algorithms/LeetCode/Solution.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+class Solutionsss {
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,1,1};
+        System.out.println(Arrays.toString((runningSum(arr))));
+    }
+    static int[] buildArray(int[] nums) {
+        int[] arr = new int[nums.length];
+        for(int i = 0; i< nums.length; i++) {
+            arr[i] = nums[nums[i]];
+        }
+        return arr;
+    }
+
+//    https://leetcode.com/problems/running-sum-of-1d-array/description/
+    static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i]+ nums[i-1];
+        }
+        return nums;
+    }
+}
