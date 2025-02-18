a997fa682bd80cd01445f6ab02ba974155943152
utakarsh23 <utakarsh.t@somaiya.edu>
2025-02-18 13:31:49 +0530

    huhuh omw

---
+++ Algorithms/LeetCode/LinkedLists/BinaryToDecimal.java
@@ -0,0 +1,32 @@
+package LeetCode.LinkedLists;
+
+public class BinaryToDecimal {
+    
+    public int getDecimalValue(ListNode head) {
+        ListNode node = head;
+        int count = 0;
+        while (node.next != null) {
+            node = node.next;
+            count++;
+        }
+        int sum = 0;
+        node = head;
+        while (node != null) {
+            int temp = node.val;
+            sum = sum + (int)Math.pow(2, count)*temp;
+            node = node.next;
+            count--;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        // Creating a linked list: 1 -> 0 -> 1 (binary 101 = decimal 5)
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(1);
+
+        BinaryToDecimal converter = new BinaryToDecimal();
+        System.out.println("Decimal value: " + converter.getDecimalValue(head)); // Output: 5
+    }
+}
--- Algorithms/LeetCode/PlusOne.java
+++ Algorithms/LeetCode/PlusOne.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
-        int[] arr = {1, 9, 9};
+        int[] arr = {8, 8, 9};
         System.out.println(Arrays.toString(plusOne(arr)));
     }
     static int[] plusOne(int[] digits) {
