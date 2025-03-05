6629d1e8e9a4222b8e1d9a61c7fd5176e0fa8f33
utakarsh23 <utakarsh.t@somaiya.edu>
2025-03-05 14:16:29 +0530

    first commit

---
+++ Algorithms/LeetCode/Solution.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+class CheckIfNumberIsASumOfPowersOfThree {
+
+    public static void main(String[] args) {
+        System.out.println(checkPowersOfThree(10));
+    }
+    static boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
